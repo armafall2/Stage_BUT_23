cf442c2602ec33c69191937e6c16f5171bb7d17a
armafall2 <mat.lur518@gmail.com>
2023-04-24 13:06:35 +0200

    Avancement 17 04

--- stage_PieuvreToqueeVueJS/src/App.vue
+++ stage_PieuvreToqueeVueJS/src/App.vue
@@ -1,34 +1,28 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Presentation from './components/Presentation.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <Presentation />
+     </div>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
+
+h1{
+  font-family: "Adlery", sans-serif;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
@@ -37,15 +31,6 @@ nav {
   text-align: center;
   margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
@@ -63,9 +48,6 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
   header .wrapper {
     display: flex;
---
+++ stage_PieuvreToqueeVueJS/src/components/Presentation.vue
@@ -0,0 +1,76 @@
+<template>
+ <div class="navbar">
+      
+      <img src="src/assets/PieuvreToquéeLogo.svg" alt="Logo" class="logo">
+    
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Devis</a></li>
+          <li><a href="#">Saumon &agrave; la coupe</a></li>
+          <li><a href="#">Calendrier</a></li>
+          <li><a href="#">En direct</a></li>
+        </ul>
+      </nav>
+    </div>
+</template>  
+
+
+<style scoped>
+@font-face {
+  font-family: "Adlery";
+  src: url("src/font/Adlery_TRIAL.ttf") format("truetype")
+}
+.navbar {
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 10px 20px;
+  box-sizing: border-box;
+}
+
+.logo {
+  height: 120px;
+  margin-right: 20px;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+}
+
+ul {
+  display: flex;
+  list-style: none;
+
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  margin-left: 20px;
+
+}
+
+a {
+  font-size: x-large;
+  color: #fff;
+  font-family: "Adlery";
+}
+
+a:hover {
+  font-weight: bold;
+  color: red;
+  padding: 5px 10px;
+
+}
+
+#decoupeBar{
+  border-style:outset 20px ;
+}
+
+</style>
